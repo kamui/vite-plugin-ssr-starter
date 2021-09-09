ad3d458a9d411156a7857979105f81886291e3bc
Jack Chu <jack@jackchu.com>
2021-09-09 04:28:15 -0400

    Add css module example

--- pages/about/index.page.tsx
+++ pages/about/index.page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./index.css";
+import classes from "./index.module.css";
 
 export { Page };
 
@@ -7,7 +7,7 @@ function Page() {
   return (
     <>
       <h1>About</h1>
-      <p>A colored page.</p>
+      <p className={classes.content}>A colored page.</p>
     </>
   );
 }
