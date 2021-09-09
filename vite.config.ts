0eff13d7cbc36fda326ad556dcb0b8a258f12758
Jack Chu <jack@jackchu.com>
2021-09-09 06:18:12 -0400

    Add vite and ts config for @ resolver alias to . path

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import ssr from "vite-plugin-ssr/plugin";
 import { UserConfig } from "vite";
+import path from "path";
 
 const config: UserConfig = {
   plugins: [reactRefresh(), ssr()],
+  resolve: {
+    alias: [{
+      find: "@",
+      replacement: path.resolve(__dirname, ".")
+    }]
+  }
 };
 
 export default config;
