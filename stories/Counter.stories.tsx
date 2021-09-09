3805a92ef789716a1a1df33600d3d9d7eb55396c
Jack Chu <jack@jackchu.com>
2021-09-09 06:20:23 -0400

    Add storybook

---
+++ stories/Counter.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Counter } from '../components/Counter';
+
+export default {
+  title: 'Example/Counter',
+  component: Counter,
+  argTypes: {
+  },
+} as ComponentMeta<typeof Counter>;
+
+const Template: ComponentStory<typeof Counter> = (args) => <Counter />;
+
+export const Default = Template.bind({});
+Default.args = {
+};
