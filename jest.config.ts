da169899385e58ae85f964cafaa08172c715d000
Jack Chu <jack@jackchu.com>
2021-09-09 22:11:06 -0400

    Add playwright e2e scaffold

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,10 @@ import type { Config } from "@jest/types"
 const config: Config.InitialOptions = {
   preset: "vite-jest",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/tests/e2e/",
+  ],
   testMatch: [
     "<rootDir>/**/__tests__/**/*.{ts,tsx}",
     "<rootDir>/**/*.{spec,test}.{ts,tsx}",
---
+++ tests/e2e/index.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/test"
+
+test("basic test", async ({ page }) => {
+  await page.goto("http://localhost:3000/")
+  const title = page.locator("title")
+  await expect(title).toHaveText("Vite SSR app")
+});
