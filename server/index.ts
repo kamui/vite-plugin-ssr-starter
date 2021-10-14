7473acc33ae5969008167775c43347b53ee21d0f
Jack Chu <jack@jackchu.com>
2021-10-14 03:43:58 -0400

    Use node clustering for application concurrency

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,28 @@
+/* eslint-disable no-console */
 import express from "express"
 import { createPageRenderer } from "vite-plugin-ssr"
+import cluster, { Worker } from "cluster"
 
 const isProduction = process.env.NODE_ENV === "production"
 const root = `${__dirname}/..`
 
-startServer()
+if (cluster.isPrimary) {
+  const workerCount = process.env.WEB_CONCURRENCY ? parseInt(process.env.WEB_CONCURRENCY, 10) : 1
+  // create a worker for each CPU
+  console.log(`forking ${workerCount} workers...`)
+
+  for (let i = 0; i < workerCount; i += 1) {
+    cluster.fork()
+  }
+
+  // When a worker dies create another one
+  cluster.on("exit", (worker: Worker) => {
+    console.log(`worker ${worker.id} exited, respawning...`)
+    cluster.fork()
+  })
+} else {
+  startServer()
+}
 
 async function startServer() {
   const app = express()
@@ -13,6 +31,7 @@ async function startServer() {
   if (isProduction) {
     app.use(express.static(`${root}/dist/client`))
   } else {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     const vite = require("vite")
     viteDevServer = await vite.createServer({
       root,
@@ -29,11 +48,13 @@ async function startServer() {
     }
     const pageContext = await renderPage(pageContextInit)
     const { httpResponse } = pageContext
-    if (!httpResponse) return next()
+    if (!httpResponse) {
+      return next()
+    }
     res.status(httpResponse.statusCode).send(httpResponse.body)
   })
 
-  const port = process.env.PORT || 3000
+  const port = process.env.PORT || "3000"
   app.listen(port)
   console.log(`Server running at http://localhost:${port}`)
 }
