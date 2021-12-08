28fe665936b3c4e3688974388e9152aee140c1c4
Jack Chu <jack@jackchu.com>
2021-12-07 23:06:07 -0500

    Update to work with latest vite-jest

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import reactRefresh from "@vitejs/plugin-react-refresh"
+import react from "@vitejs/plugin-react"
 import ssr from "vite-plugin-ssr/plugin"
 import { UserConfig } from "vite"
 import path from "path"
 
+const nodeEnv = process.env.NODE_ENV || ""
+
 const config: UserConfig = {
-  plugins: [...(process.env.NODE_ENV !== "test" ? [reactRefresh()] : []), ssr()],
+  plugins: [
+    react({
+      fastRefresh: !["test", "production"].includes(nodeEnv),
+    }),
+    ssr(),
+  ],
   resolve: {
     alias: {
       "#app": path.resolve(__dirname, "."),
